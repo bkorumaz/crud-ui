fedb04e8156ebcb1a7d9965ff0a667c4dd37dd3d
Bahadır Korumaz <bahadir.korumaz@gmail.com>
2022-02-02 00:46:09 +0300

    Add empty nav component and add styling to background component

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,9 +1,13 @@
 import React from "react";
 import BackgroundCSS from "./Background.module.css"
+import Oval from "../Oval/Oval";
 
-const Background = ({children}) => {
+const Background = () => {
     return(
-        <div className={BackgroundCSS.BG}/>
+        <div className={"Background"}>
+            <Oval/>
+            <div className={BackgroundCSS.BG}/>
+        </div>
     );
 };
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,12 @@
 import Background from "../Background/Background";
 import React from "react";
 import Oval from "../Oval/Oval";
+import Nav from "../Nav/Nav";
 
 const Header = () => {
     return(
        <div className={"Header"}>
-           {/*<Nav/>*/}
-           <Oval/>
-           <Oval/>
-           <Oval/>
+           <Nav/>
            <Background/>
        </div>
     )
